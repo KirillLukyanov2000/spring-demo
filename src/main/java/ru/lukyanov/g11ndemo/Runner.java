f8f5cac07c6c650a9bcd6160700fef76d7dce34d
Kirill Lukyanov <101703819+KirillLukyanov2000@users.noreply.github.com>
2024-04-04 15:07:23 +0300

    Update Runner.java

--- src/main/java/ru/lukyanov/g11ndemo/Runner.java
+++ src/main/java/ru/lukyanov/g11ndemo/Runner.java
@@ -1,14 +1,16 @@
+
 package ru.lukyanov.g11ndemo;
 
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 public class Runner {
 
  public static void main(String[] args) {
-   Locale ru = new Locale("ru");
-   ResourceBundle strSource = ResourceBundle.getBundle("lang", ru);
-   String greeting = strSource.getString("greeting");
-   System.out.println(greeting);                                                
+   ResourceManager translator = ResourceManager.INSTANCE;
+   Locale locale = new Locale("ru", "RU");
+   translator.setLocale(locale);
+   System.out.println(translator.get(Message.GREETING) + " " + translator.get(Message.QUESTION));
+   System.out.println(translator.get(User.FIRST_NAME) + " " + translator.get(User.LAST_NAME));   
  }
+ 
 }
